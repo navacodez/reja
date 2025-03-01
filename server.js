1d81547c9d9bb3eb895303acbc82d7f18f43732a
navacodez <navacodez@gmail.com>
2025-03-02 07:19:15 +0900

    added harid.ejs

--- server.js
+++ server.js
@@ -1,2 +1,32 @@
-console.log("web serverni bosh");
+console.log("Web Serverni boshlash");
 const express = require("express");
+const app = express();
+const http = require("http");
+
+// 1: Kirish code
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+// 2: Session code
+// 3: Views code
+app.set("views", "views");
+app.set("view engine", "ejs");
+
+// 4: Routing code
+app.post("/create-item", (req, res) => {
+  console.log(req.body);
+  // console.log(req);
+  res.json({ test: "success"});
+});
+
+
+app.get("/", function (req, res) {
+  res.render("harid");
+});
+
+const server = http.createServer(app);
+let PORT = 3000;
+server.listen(PORT, function() {
+  console.log(`The server is running successfully on port: ${PORT}`);
+});
